ca46f08f201bec94e776b3861b0c96ad53a9d6ce
Eric Hilary Smith <erichilarysmith@outlook.com>
2020-03-27 11:34:19 -0500

    Add files via upload

---
+++ Random Glucose Levels Game.js
@@ -0,0 +1,190 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// always-run-in-app: true; icon-color: deep-brown;
+// icon-glyph: chess-board; share-sheet-inputs: plain-text, url, file-url, image;
+// COSINE
+// TRANCE Health Promotions
+// SEELAH Media Works
+// Kingdom of TRANCE
+// Hysteria P TRANCE
+var RandomGlucoseLevel;
+RandomGlucoseLevel;
+var gameplayerScore;
+gameplayerScore;
+var RandomGlucoseLevelString = "Public Nuisance City.";
+var RandomGlucoseLevel = ["Fitbit", "Dexcom", "patientComplications", "patientNoComplications"];
+var populationBlack;
+populationBlack;
+var populationWhite;
+populationWhite;
+var populationLatino;
+populationLatino;
+var patientAge= 55;
+patientAge;
+patientAge = 55;
+var populationBlack = true;
+var populationWhite = false;
+populationBlack && populationWhite;
+var populationBlack = true;
+var populationLatino = false;
+populationBlack && populationLatino;
+var populationBlack = true;
+var populationWhite = false;
+populationBlack || populationWhite;
+var populationBlack = true;
+var populationLatino = false;
+populationBlack || populationLatino;
+var patientAgeRestriction = 62;
+patientAge < patientAgeRestriction;
+var nichePublicNuisanceCityPark;
+nichePublicNuisanceCityPark;
+var FemaleAcquaintance;
+FemaleAcquaintance;
+var MaleAcquaintance;
+MaleAcquaintance;
+var patchEnglewood;
+patchEnglewood;
+var patchEnglewood = ["populationWhite", "populationBlack", "populationLatino", "FemaleAcquaintance", "MaleAcquaintance"];
+var patientNoComplications;
+patientNoComplications;
+var patientComplications;
+patientComplications;
+var nonplayerLifeShocks;
+nonplayerLifeShocks;
+var unitsLifeShocks;
+unitsLifeShocks;
+var patchEnglewood = "playerFemale";
+console.log("Welcome " + patchEnglewood);
+if (patchEnglewood.length > 11) {
+  console.log("Some number of natural mortality rate.");
+  }
+  var patchEnglewood = "playerMale";
+  console.log("Welcome " + patchEnglewood);
+  if (patchEnglewood.length > 9) {
+    console.log("Some number of probability of developing some complications.");
+    }
+    var ViciousCyclePoverty;
+    ViciousCyclePoverty;
+    var ViciousCyclePoverty = ["playerMissPeggyPit", "playerBarryUKolmar", "playerProgenitor", "playerOffSpringMale", "playerOffSpringFemale"];
+    var playerCharacter;
+    playerCharacter;
+    var playerMale;
+    playerMale;
+    var playerFemale;
+    playerFemale;
+    var playerMale = true;
+    var playerFemale = true;
+    playerMale || playerFemale;
+    var playerUnitBMI;
+    playerUnitBMI;
+    var playerUnitBMIFunction = function () {
+      console.log("Each level in the some number player BMI score is set to B random levels from 0.001 and 99.999.");
+      };
+    var playerHeight;
+    playerHeight;
+    var playerWeight;
+    playerWeight;
+    playerWeight / playerHeight * 703;
+    var playerMissPeggyPit;
+    playerMissPeggyPit;
+    var ViciousCyclePoverty = "playerMissPeggyPit";
+    console.log("Welcome " + ViciousCyclePoverty);
+    if (ViciousCyclePoverty.length > 17) {
+      console.log("The business associate of the Pit Room.");
+      }
+    var playerMissPeggyPitString = "Is a Female Acquaintance with patchEnglewood.";
+    var playerBarryUKolmar;
+    playerBarryUKolmar;
+    var ViciousCyclePoverty = "playerBarryUKolmar";
+    console.log("Welcome " + ViciousCyclePoverty);
+    if (ViciousCyclePoverty.length > 17) {
+      console.log("Red car taxi driver from Public Nuisance City Cab Service.");
+      }
+    var playerBarryUKolmarString = "Is a Male Acquaintance with patchEnglewood.";
+    var playerProgenitor;
+    playerProgenitor;
+    var ViciousCyclePoverty = "playerProgenitor";
+    console.log("Welcome " + ViciousCyclePoverty);
+    if (ViciousCyclePoverty.length > 15) {
+      console.log("Has some number of male and female off spring in Englewood.");
+      }
+    var playerProgenitorString = "Has no resources in the Englewood patch.";
+    var playerOffSpringMale;
+    playerOffSpringMale;
+    var ViciousCyclePoverty = "playerOffSpringMale";
+    console.log("Welcome " + ViciousCyclePoverty);
+    if (ViciousCyclePoverty.length > 18) {
+      console.log("Is one of the male off spring of the progenitor.");
+      }
+      var playerOffSpringMaleString = "Left the Englewood patch because Resource Depletion.";
+    var playerOffSpringFemale;
+    playerOffSpringFemale;
+    var ViciousCyclePoverty = "playerOffSpringFemale";
+    console.log("Welcome " + ViciousCyclePoverty);
+    if (ViciousCyclePoverty.length > 20) {
+      console.log("Is one of the female off spring of the progenitor.");
+      }
+      var playerOffSpringFemaleString = "Left the Englewood patch because the lack of Parental Investment.";
+      var nonplayerCharacter;
+      nonplayerCharacter;
+      var nonplayerPredatorOEDIPUSX;
+      nonplayerPredatorOEDIPUSX;
+      var nonplayerPredatorBlackPalGED;
+      nonplayerPredatorBlackPalGED;
+      var BlackPalGEDString = "He is a THUG.";
+      var BlackPalGED = "THUG";
+      for (var i = 0;  i < BlackPalGED.length;  i++) {
+        console.log("Black Pal GED is a " + BlackPalGED[i] + ".");
+        }
+        var nicheDaPigBar;
+        nicheDaPigBar;
+        var nonplayerHysteriaPTranceED;
+      nonplayerHysteriaPTranceED;
+      var nonplayerDoctorFROCK;
+      nonplayerDoctorFROCK;
+      var patchAustin;
+      patchAustin;
+      var nicheSophoclesBar;
+      nicheSophoclesBar;
+      var nicheHazardousDutyBar;
+      nicheHazardousDutyBar;
+      var patchUptown;
+      patchUptown;
+      var GerryManderString = "He is friends with Black Pal GED.";
+      var playerLeavingCosts;
+      playerLeavingCosts;
+      var playerRisk = "playerUGT";
+      console.log("Welcome " + playerRisk);
+      if (playerRisk.length > 8) {
+        console.log("The player leaves the Englewood patch, then the player's BMI score will drop 8 levels because of UGT sickness.");
+        }
+      var playerVisitingCosts;
+      playerVisitingCosts;
+      var playerSwitchingCosts;
+      playerSwitchingCosts;
+      var playerTimeHandling;
+      playerTimeHandling;
+      var playerTimeSearch;
+      playerTimeSearch;
+      var playerForagingCosts;
+      playerForagingCosts;
+      var gameResources;
+      gameResources;
+      var playerResources;
+      playerResources;
+      var resourcesFood;
+      resourcesFood;
+      var resourcesMarshmallows;
+      resourcesMarshmallows;
+      var playerMarshmallows;
+      playerMarshmallows;
+      var playerEnergy;
+      playerEnergy;
+      var energyMarshmallows;
+      energyMarshmallows;
+      var playerFitness;
+      playerFitness;
+      var playerRisk;
+      playerRisk;
+      var riskUGT;
+      riskUGT;
